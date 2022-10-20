29c742ad070932270c07f96b7397c64360567ec9
dev-syn <103451081+dev-syn@users.noreply.github.com>
2022-10-20 05:49:41 -0400

    Improve class description

--- lib/init.lua
+++ lib/init.lua
@@ -5,7 +5,7 @@ export type TextStyling = Types.TextStyling;
 --[=[
     @class TextStyling
 
-    This class was designed to simplify the colours used in text and allows you to use colour codes which then are converted into font tags for rich text.
+    This class was designed to simplify the colours used in text and allows you to use colour & format codes which then are converted into font tags for rich text.
 
     ## Colour Codes
         &0 - Black
