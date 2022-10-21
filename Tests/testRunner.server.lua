116f3c7e52be8437735e753231339425378643d9
dev-syn <103451081+dev-syn@users.noreply.github.com>
2022-10-20 21:15:58 -0400

    Update test for same colour wrapping

--- Tests/testRunner.server.lua
+++ Tests/testRunner.server.lua
@@ -3,5 +3,7 @@ if TextStylingModule then
     ---@module lib/Types
     local Types = require(TextStylingModule:FindFirstChild("Types"));
     local TextStyling: Types.TextStyling = require(TextStylingModule);
-    
+    local str: string = "&f&l[&rcccmdm&f&l]&r:";
+    print(str);
+    print(TextStyling.ParseTextCodes(str));
 end
